8edcaa9ab7e46f4ceee0bc6ca53a01bab1329f16
Muhammadarif98 <67822369+Muhammadarif98@users.noreply.github.com>
2020-11-20 18:55:23 +0300

    ret

--- app/src/main/java/com/example/ihsan/HomeActivity.java
+++ app/src/main/java/com/example/ihsan/HomeActivity.java
@@ -2,7 +2,9 @@ package com.example.ihsan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -12,6 +14,14 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
         //Чисто картинка на передний экран пару секунд и все
 
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(HomeActivity.this,SplashActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
 
 
 
